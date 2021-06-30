4eb0a06ba3e097fb1354f8401d22c978f4d20eb4
tylermatrix <tylermaatrix@gmail.com>
2021-06-29 20:48:37 -0700

    added cleanup function to the useEffect call, add UI elements so user can follow article to wiki link

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,25 +8,49 @@ const Search = () => {
 
 
   useEffect(() => {
+
+    
    const search = async () => {
-   const {data} =  await axios.get('https://en.wikipedia.org/w/api.php', {
-      params: {
-        action: 'query',
-        list: 'search',
-        origin: '*',
-        format: 'json',
-        srsearch: term
-      },
-    });
-    setResults(data.query.search);
+    const {data} =  await axios.get('https://en.wikipedia.org/w/api.php', {
+       params: {
+         action: 'query',
+         list: 'search',
+         origin: '*',
+         format: 'json',
+         srsearch: term
+       },
+     });
+     setResults(data.query.search);
+    }
+ 
+   const timeoutId = setTimeout(() => {
+     if (term) {
+       search();
+      }
+   }, 500)
+
+   return () => {
+     clearTimeout(timeoutId);
    }
 
-   if (term) {
-    search();
-   }
-  
+
+
   }, [term]);
 
+
+  const renderedResults = results.map((result) => {
+    return (<div key={result.pageid} className="item">
+      <div className="right floated content">
+        <a href={`https://en.wikipedia.org?curid=${result.pageid}`} className="ui button" >Go</a>
+      </div>
+      <div className="content">
+        <div className="header">
+          {result.title}
+        </div>
+        <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+      </div>
+    </div>)
+  })
 
   return(
     <div>
@@ -40,6 +64,9 @@ const Search = () => {
           onChange={e => setTerm(e.target.value)}
           />
         </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
   )
